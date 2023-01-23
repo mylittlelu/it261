954092c2412f3638d3580d4acdbe9891c39f9c1a
mylittlelu <122073118+mylittlelu@users.noreply.github.com>
2023-01-22 20:19:48 -0800

    Add files via upload

---
+++ website/index.php
@@ -0,0 +1,53 @@
+<?php
+include('./includes/header.inc');
+?>
+
+<div id="wrapper">
+
+<!-- <div id="hero">
+<img src="images/lamb75.gif" alt="Otis">
+
+</div> -->
+<!--end hero-->
+
+<main>
+<h1>Welcome to the Web App Programming Course!</h1>
+<h2>This is where we learn PHP</h2>
+<p>
+
+    Lorem ipsum dolor sit amet. Ex harum molestiae id laborum velit aut recusandae delectus. Ex molestiae sint ex corrupti quis vel fugit sint ut consequatur doloremque et rerum enim eos natus sequi. Aut velit facilis et rerum eveniet et deleniti cupiditate. Id iusto quia et magnam nulla qui iure illum cum corporis quos et consectetur tempore ea assumenda ipsum eos illo expedita.
+    
+    Qui voluptas repellendus hic alias nulla 33 numquam quis aut maxime internos et beatae natus non unde voluptas nam inventore facilis. Ad provident earum sed dolore nulla et sint galisum ut harum possimus.
+    
+    Ut facere voluptas qui fugiat distinctio est nisi aliquam nam maiores alias ex sequi voluptates et voluptas dolores qui sint consequatur. Eum nemo voluptatibus et quas deleniti est dolores explicabo vel veritatis ipsum et voluptatibus incidunt ut vero veritatis ab voluptates doloribus.
+    </p>
+    <h2>another headline</h2>
+    <p>
+        
+
+Lorem ipsum dolor sit amet. Ex harum molestiae id laborum velit aut recusandae delectus. Ex molestiae sint ex corrupti quis vel fugit sint ut consequatur doloremque et rerum enim eos natus sequi. Aut velit facilis et rerum eveniet et deleniti cupiditate. Id iusto quia et magnam nulla qui iure illum cum corporis quos et consectetur tempore ea assumenda ipsum eos illo expedita.
+
+Qui voluptas repellendus hic alias nulla 33 numquam quis aut maxime internos et beatae natus non unde voluptas nam inventore facilis. Ad provident earum sed dolore nulla et sint galisum ut harum possimus.
+
+Ut facere voluptas qui fugiat distinctio est nisi aliquam nam maiores alias ex sequi voluptates et voluptas dolores qui sint consequatur. Eum nemo voluptatibus et quas deleniti est dolores explicabo vel veritatis ipsum et voluptatibus incidunt ut vero veritatis ab voluptates doloribus.
+
+    </p>
+</main>
+
+<aside>
+<h3>Headline 3 in Aside</h3>
+<p>
+
+    Lorem ipsum dolor sit amet. Ex harum molestiae id laborum velit aut recusandae delectus. Ex molestiae sint ex corrupti quis vel fugit sint ut consequatur doloremque et rerum enim eos natus sequi. Aut velit facilis et rerum eveniet et deleniti cupiditate. Id iusto quia et magnam nulla qui iure illum cum corporis quos et consectetur tempore ea assumenda ipsum eos illo expedita.
+    
+    Qui voluptas repellendus hic alias nulla 33 numquam quis aut maxime internos et beatae natus non unde voluptas nam inventore facilis. Ad provident earum sed dolore nulla et sint galisum ut harum possimus.
+    
+    Ut facere voluptas qui fugiat distinctio est nisi aliquam nam maiores alias ex sequi voluptates et voluptas dolores qui sint consequatur. Eum nemo voluptatibus et quas deleniti est dolores explicabo vel veritatis ipsum et voluptatibus incidunt ut vero veritatis ab voluptates doloribus.
+    </p>
+</aside>
+
+</div>
+<!--end wrapper-->
+
+<?php
+include('./includes/footer.inc');

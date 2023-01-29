032a55389b5f32c04f611a6281653559bed6a03a
mylittlelu <122073118+mylittlelu@users.noreply.github.com>
2023-01-29 00:50:01 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -68,6 +68,16 @@
     <li><a href="weeks/week3/date.php">date</a></li>
     <li><a href="weeks/week3/switch.php">switch</a></li>
 </ul>
+<h3>Week 4</h3>
+<ul>
+	<li><a href="weeks/week4/form-get.php">form-get.php</a></li>
+    <li><a href="weeks/week4/form1.php">form1.php</a></li>
+    <li><a href="weeks/week4/form2.php">form2.php</a></li>
+    <li><a href="weeks/week4/form3.php">form3.php</a></li>
+    <li><a href="weeks/week4/arithmetic-form.php">arithmetic-form.php</a></li>
+    <li><a href="weeks/week4/celsius.php">celsius.php</a></li>
+	<li><a href="weeks/week4/adder.php" style="font-size:10px;">(...and the corrected adder page)</a></li>
+</ul>
 </aside>
 
 

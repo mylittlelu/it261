4f823b25acc78f51c5a5ad20cef8b5d19039a56a
mylittlelu <122073118+mylittlelu@users.noreply.github.com>
2023-02-12 23:58:52 -0800

    Add files via upload

--- weeks/week6/thx.php
+++ weeks/week6/thx.php
@@ -1,12 +1,14 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thanks</title>
+    <title>Thank You</title>
+    <link href="css/styles.css" type="text/css" rel="stylesheet">
 </head>
 <body>
-    thanks
+<h1>Thank you for your submission!</h1>
 </body>
 </html>

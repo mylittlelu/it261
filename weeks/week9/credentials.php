5f93828c81af18d368ccf97821aed47c01cb55ea
mylittlelu <122073118+mylittlelu@users.noreply.github.com>
2023-03-04 00:22:32 -0800

    Add files via upload

---
+++ weeks/week9/credentials.php
@@ -0,0 +1,20 @@
+<?php
+
+// avanv_school_projects -- database name
+// school_proj_user -- name of user (server has name length limit)
+// p: yiJB27fmChYUbbu
+
+// use this set if using UW server
+// define('DB_NAME', 'school_projects');
+// define('DB_USER', 'school_proj_user');
+// define('DB_PASSWORD', 'yiJB27fmChYUbbu');
+// define('DB_HOST', 'avanv.ovid.u.washington.edu');
+// define('DB_PORT', 4234);
+
+// use this set if using dreamhost
+define('DB_NAME', 'avanv_school_projects');
+define('DB_USER', 'school_proj_user');
+define('DB_PASSWORD', 'yiJB27fmChYUbbu');
+define('DB_HOST', 'mysql.avanv.com');
+
+ 
